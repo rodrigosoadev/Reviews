8eb9695c84a70dfe05952f13111e7db167ac892b
Rodrigo Soares <meuemail.rodrigodev@gmail.com>
2022-09-17 21:27:39 -0300

    deploy

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Reviews from './components/Review'
+
+function App() {
+  return (
+   <main>
+    <section className='container'>
+      <div className="title">
+        <h2>our reviews</h2>
+        <div className="underline"></div>
+      </div>
+      <Reviews/>
+    </section>
+   </main>
+  )
+}
+
+export default App
